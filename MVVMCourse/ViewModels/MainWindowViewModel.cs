f0b5fc525b1dec8023cd906ea14ec1d67d3cd072
Кирилл Новиков <GreezXII@protonmail.ch>
2022-04-23 22:44:54 +0300

    ViewModel test

---
+++ MVVMCourse/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MVVMCourse.ViewModels.Base;
+
+namespace MVVMCourse.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Title
+        /// <summary> Window title </summary>
+        private string _Title = "Анализ статистики";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
