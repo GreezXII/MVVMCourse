3e463749c68f8100dbd01966355225da37086b62
Кирилл Новиков <GreezXII@protonmail.ch>
2022-04-24 19:53:00 +0300

    Implement AboutCommand

---
+++ MVVMCourse/Infrastructure/Commands/AboutCommand.cs
@@ -0,0 +1,19 @@
+﻿using MVVMCourse.Infrastructure.Commands.Base;
+using System.Windows;
+using System;
+
+namespace MVVMCourse.Infrastructure.Commands
+{
+    class AboutCommand : Command
+    {
+        private string _About = string.Join(
+            Environment.NewLine,
+            "This is test MVVM project.",
+            "Course author: Pavel Shmachiln.",
+            "Student: me, lol.");
+
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => MessageBox.Show(_About, "About program", MessageBoxButton.OK);
+    }
+}
