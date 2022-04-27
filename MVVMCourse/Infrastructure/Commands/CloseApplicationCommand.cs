95dfcc278c5d604dbe47d1edab791b5a45b1649e
Кирилл Новиков <GreezXII@protonmail.ch>
2022-04-27 20:32:08 +0300

    Implement commands as static resources

---
+++ MVVMCourse/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVMCourse.Infrastructure.Commands
+{
+    class CloseApplicationCommand : Base.Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => App.Current.Shutdown();
+    }
+}
